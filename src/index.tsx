5e9c8b6987b689e29725a445107c2db920b38691
ElenaVlasenko <radionovaelena124@gmail.com>
2024-02-23 02:00:32 +0300

    pages were added

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './app';
+import { Setting } from './const';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <h1>Hello, World!</h1>
-  </React.StrictMode>
+  <App
+    offersCount={Setting.OffersCount}
+  />
 );
